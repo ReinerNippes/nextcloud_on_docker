e5c050132ae46efc41932c2bb8c08eff7cfaa5cd
Reiner Nippes <reiner@nippes.it>
2019-09-28 19:38:20 +0000

    fixed issue #13

--- scripts/remove_all_container.sh
+++ scripts/remove_all_container.sh
@@ -3,5 +3,4 @@
 docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
-docker volume rm $(docker volume ls -q)
-docker network rm $(docker network ls -q | grep -v bridge | grep -v host | grep -v none)
+
